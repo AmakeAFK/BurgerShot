b9b2e088ed65f8c3503344b8078832d4f6c7e5c6
AmakeAFK <82289407+AmakeAFK@users.noreply.github.com>
2025-02-26 15:12:47 +0100

    Update script.js

--- script.js
+++ script.js
@@ -8,9 +8,9 @@ function showDetails(category) {
             content = `
                 <img src="imagenes/burgerShot_baconlov.png" alt="Hamburguesa Quecu Infarto">
                 <h2>Hamburguesa Quecu Infarto</h2>
-                <p>¡Irresistible!</p>
+                <p>Una hamburguesa que te ofrecera una experiencia completa por el hospital.</p>
                 <ul>
-                    <li>Quecu Infarto - $50</li>
+                    <li>Quecu Infarto - $65</li>
                 </ul>
             `;
             break;
@@ -18,19 +18,19 @@ function showDetails(category) {
             content = `
                 <img src="imagenes/BurgerShot_MenuKid.png" alt="Cajita Feliz">
                 <h2>Cajita Feliz</h2>
-                <p>Por poner.</p>
+                <p>El menú perfecto para las nuevas juventudes</p>
                 <ul>
-                    <li>Cajita Feliz - $40</li>
+                    <li>Cajita Feliz - $50</li>
                 </ul>
             `;
             break;
         case 'burrito':
             content = `
-                <img src="imagenes/burgerShot_buritos.png" alt="Burrito de Pollo">
+                <img src="imagenes/burgerShot_buritos.png" alt="Burrito">
                 <h2>Burrito</h2>
-                <p>Por poner.</p>
+                <p>Burrito de carne muyyyyyyy bueno</p>
                 <ul>
-                    <li>Burrito - $40</li>
+                    <li>Burrito - $50</li>
                 </ul>
             `;
             break;
@@ -38,13 +38,13 @@ function showDetails(category) {
             content = `
                 <img src="imagenes/burgerShot_BuritosChicken.png" alt="Burrito de Pollo">
                 <h2>Burrito de pollo</h2>
-                <p>Por poner.</p>
+                <p>Burrito de pollo muyyyyyyy bueno</p>
                 <ul>
-                    <li>Burrito de pollo - $40</li>
+                    <li>Burrito de pollo - $50</li>
                 </ul>
             `;
             break;
-        case 'comboEspecial':
+        case 'cheeseburgers':
             content = `
                 <h2>Cheeseburgers - Elige tu favorita</h2>
                 <div class="combo-sections">
@@ -58,13 +58,13 @@ function showDetails(category) {
                         <img src="imagenes/burgerShot_cheese2.png" alt="Doble Cheeseburger">
                         <h3>Doble Cheeseburger</h3>
                         <p>Doble Cheeseburger</p>
-                        <p class="price">$50</p>
+                        <p class="price">$65</p>
                     </div>
                     <div class="combo-section">
                         <img src="imagenes/burgerShot_cheese3.png" alt="Triple Cheeseburger">
                         <h3>Triple Cheeseburger</h3>
                         <p>Triple Cheeseburger</p>
-                        <p class="price">$50</p>
+                        <p class="price">$70</p>
                     </div>
                 </div>
             `;
